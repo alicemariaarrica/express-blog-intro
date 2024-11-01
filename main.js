67a3c74bafb38bdb8f658e4ac34bec80960563d2
alicemariaarrica <alicemariaarrica@gmail.com>
2024-11-01 22:35:35 +0100

    const-commit

---
+++ main.js
@@ -0,0 +1,9 @@
+//Creiamo il progetto base con una rotta / che ritorna un h1 con scritto Benvenuto nel mio blog!
+
+const express = require("express")
+
+const app = express()
+
+const port = 3333
+
+const host = "http://127.0.0.1:"
