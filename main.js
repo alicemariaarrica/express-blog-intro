5269a10f026d6cd36c131857e96e49b05c385942
alicemariaarrica <alicemariaarrica@gmail.com>
2024-11-01 23:04:21 +0100

    adj2-commit

--- main.js
+++ main.js
@@ -1,22 +1,13 @@
 //Creiamo il progetto base con una rotta / che ritorna un h1 con scritto Benvenuto nel mio blog!
 
-const express = require("express")
+const express = require("express"); 
 
-const app = express()
+const app = express(); 
 
-const port = 3333
+const port = 3333;
 
-const localhost = "http://127.0.0.1:"
+app.get("/", (req, res) => {
+  res.send("<h1>Benvenuto nel mio blog!</h1>");
+});
 
-
-
-app.use(express.static("public"))
-
-app.get("/", (req, res) =>{
-    res.send("<h1>Benvenuto nel mio blog!</h1>")
-})
-
-app.listen(port, ()=>{
-    console.log(`Server run on:${localhost}${port}`);
-    
-})
+//Creiamo un array dove inserire una lista di almeno 5 post, per ognuno indicare titolo, contenuto, immagine e tags (tags è un array di stringhe)
